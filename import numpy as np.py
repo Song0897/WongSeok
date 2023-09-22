42255e0775cb479138b2405a3ba8248f7990edec
Your Name <gonzoxoco@gmail.com>
2023-09-22 14:20:45 +0900

    S_0001

--- import numpy as np.py
+++ import numpy as np.py
@@ -1,23 +1,38 @@
-import numpy as np
-import pandas as pd
+# 내업무 자동화
 
-1
-2
-3
+import pyodbc
 
+# 데이터베이스 연결 정보 설정
+server = '데이터베이스 엔진'  # SQL Server의 주소 또는 이름
+database = '210.180.103.100'  # 연결할 데이터베이스 이름
+username = 'hns_prm'  # SQL Server 사용자명
+password = '@tjqltmrhksflxla1'  # 사용자의 비밀번호
 
-test = 1
+# 연결 문자열 생성
+connection_string = f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
-print ("Hello World")
+# 데이터베이스 연결
+try:
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
 
-print ("Hello World")
+    # SQL 쿼리 파일 읽기
+    with open('pol_20230911.sql', 'r') as sql_file:
+        sql_query = sql_file.read()
 
-test
+    # SQL 쿼리 실행
+    cursor.execute(sql_query)
 
-# git config --global user.email "gonzoxoco@gmail.com"
-# git config --global user.name "Song0897"
-print ("Hello World")
+    # 결과 가져오기
+    rows = cursor.fetchall()
+    
+    # 결과 출력 예제
+    for row in rows:
+        print(row)
 
+    # 연결 종료
+    cursor.close()
+    connection.close()
 
-print ("Hello World")
-print ("Hello World")
+except Exception as e:
+    print(f"Error: {str(e)}")
