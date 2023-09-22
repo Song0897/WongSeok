25a7766c9a51851e6be799c6ef8de15fb5d1cd38
Your Name <gonzoxoco@gmail.com>
2023-09-22 14:11:55 +0900

    test003

--- import numpy as np.py
+++ import numpy as np.py
@@ -10,7 +10,10 @@ test = 1
 
 print ("Hello World")
 
+print ("Hello World")
+
 test
 
 # git config --global user.email "gonzoxoco@gmail.com"
 # git config --global user.name "Song0897"
+print ("Hello World")
